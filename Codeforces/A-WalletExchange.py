0af066b54af41ae20fc5fd8a75e90424a515933b
Anushka Priyadarshi <anushka@Anushkas-MacBook-Air-2.local>
2024-01-06 22:08:03 +0530

    A-WalletExchange

---
+++ Codeforces/A-WalletExchange.py
@@ -0,0 +1,14 @@
+t=int(input())
+for i in range(t):
+    a,b=list(map(int,input().split()))
+    sum=a+b
+    if sum%2==0:
+        print("Bob")
+    else:
+        print("Alice")
+
+        
+   
+        
+
+            
