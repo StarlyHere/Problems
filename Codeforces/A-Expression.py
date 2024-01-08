0340a50b33ad60a62683c8891ee6c020f1d72b3e
Anushka Priyadarshi <anushka1665@gmail.com>
2024-01-08 19:19:08 +0530

    A-Expression

--- Codeforces/A-Expression.py
+++ Codeforces/A-Expression.py
@@ -1,7 +1,5 @@
 a=int(input())
 b=int(input())
 c=int(input())
-results = [
-    a + (b * c),a * (b + c),a * b * c,(a + b) * c,a + b + c]
-
+results = [a + (b * c),a * (b + c),a * b * c,(a + b) * c,a + b + c]
 print(max(results))
