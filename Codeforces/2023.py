4737e132953f60631e306d8c79e21f482f5a09be
Anushka Priyadarshi <anushka@Anushkas-MacBook-Air-2.local>
2024-01-08 18:54:30 +0530

    A-2023

---
+++ Codeforces/2023.py
@@ -0,0 +1,21 @@
+t=int(input())
+for i in range(t):
+    n,b=list(map(int,input().split()))
+    a=list(map(int,input().split()))
+    product=1
+    ls=[]
+    for i in range(n):
+        product*=a[i]
+
+    if 2023%product==0:
+        print("YES")
+        ls.append(2023//product)
+        for i in range(b-len(ls)):
+            ls.append(1)
+        for i in range(len(ls)):
+            print(ls[i], end=" ")      
+    else:
+        print("NO")
+    
+
+
