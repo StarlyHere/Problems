8884e25f4e53767058ede8238eb45ad47d869025
Anushka Priyadarshi <anushka@Anushkas-MacBook-Air-2.local>
2024-01-08 19:07:03 +0530

    changes

--- Codeforces/A-WalletExchange.py
+++ Codeforces/A-WalletExchange.py
@@ -1,11 +1,21 @@
-t=int(input())
-for i in range(t):
-    a,b=list(map(int,input().split()))
-    sum=a+b
-    if sum%2==0:
-        print("Bob")
-    else:
-        print("Alice")
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    v = list(map(int, input().split()))
+    a, b, cnt = float('inf'), float('inf'), 0
+    for i in v:
+        if a > b:
+            a, b = b, a
+        if i <= a:
+            a = i
+        elif i <= b:
+            b = i
+        else:
+            a = i
+            cnt += 1
+    
+    print(cnt)
+
 
         
    
